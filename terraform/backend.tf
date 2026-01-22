5a019098bb68e988b2ac3073bb5144843a8e55fc
Alisonnnmichella <Alisonnnmichella@users.noreply.github.com>
2026-01-22 17:11:41 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "azurepilotdeploy"
+    storage_account_name = "pilotstateaccount"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
 
