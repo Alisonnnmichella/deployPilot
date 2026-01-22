c013915e787455599ee6bda204e2e22a2af82eda
Alisonnnmichella <Alisonnnmichella@users.noreply.github.com>
2026-01-22 13:28:56 -0300

    Add Azure backend configuration for Terraform

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "azurepilotdeploy"
+    storage_account_name = "pilotstateaccount"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
