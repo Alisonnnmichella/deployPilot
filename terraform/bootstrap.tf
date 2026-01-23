d41dfde5a3186984d086608fd31847d16d256a40
Alisonnnmichella <alisonnnmichella@gmail.com>
2026-01-22 23:37:28 -0300

    not duplicate

--- terraform/bootstrap.tf
+++ terraform/bootstrap.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group
   location = var.location
