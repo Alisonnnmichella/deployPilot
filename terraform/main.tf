013c65e6a0c530c936c24a05392cb3e681341c57
Alisonnnmichella <alisonnnmichella@gmail.com>
2026-01-22 23:51:07 -0300

    keep status

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 data "azurerm_resource_group" "main" { 
   name = "pilot-tfstate-rg" 
 }
